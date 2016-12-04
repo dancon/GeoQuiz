e9118e45c3ed19242ce16ebae6161bd2b96e1019
John Hou <870301137@qq.com>
2016-12-04 20:20:19 +0800

    我的第一个 android 程序

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -0,0 +1,114 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
+    private TextView mQuestionTextView;
+
+    private int mCurrentIndex = 1;
+
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
+
+    // 更新问题
+    private void updateQuestion(boolean isNext){
+        if(isNext){
+            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+        }else{
+            mCurrentIndex -= mCurrentIndex;
+            if(mCurrentIndex < 0){
+                mCurrentIndex = mCurrentIndex + mQuestionBank.length;
+            }
+        }
+
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    // 检查问题正确性, 如果题目正确就跳转道下一题
+    private void checkAnswer(boolean userPressedTrue){
+        boolean anserIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+        if (userPressedTrue == anserIsTrue){
+            messageResId = R.string.correct_toast;
+            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+            updateQuestion(true);
+        }else{
+            messageResId = R.string.incorrect_toast;
+            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // 在 Activity 启动的时候首先会触发的事件
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        // 通过资源 id 获取组件
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        mQuestionTextView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                updateQuestion(true);
+            }
+        });
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+
+        // 设置监听事件
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                checkAnswer(true);
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+        mFalseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                checkAnswer(false);
+            }
+        });
+
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
+
+        mNextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                updateQuestion(true);
+            }
+        });
+
+        // 上一题
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                updateQuestion(false);
+            }
+        });
+
+        updateQuestion(false);
+    }
+}
